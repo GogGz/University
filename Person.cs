38838e5cdc098e9e37e07fefedf118a63e761839
GogGz <120939822+GogGz@users.noreply.github.com>
2023-01-11 05:55:25 +0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace University
+{
+	public class Person
+	{
+		public Person()
+		{
+		}
+	}
+}
+
---
+++ Professor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace University
+{
+	public class Professor
+	{
+		public Professor()
+		{
+		}
+	}
+}
+
